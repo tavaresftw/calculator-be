a3a66e5500b7b90d0864f08d7280adb6049013ea
Jair Alves Tavares Junior <junior.age@gmail.com>
2024-08-27 18:57:55 -0300

    ADJUST REDIS FOR HEROKU

--- src/main/java/com/ntd/calculator/config/RedisConfig.java
+++ src/main/java/com/ntd/calculator/config/RedisConfig.java
@@ -2,16 +2,32 @@ package com.ntd.calculator.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("redis", 6379);
+    public LettuceConnectionFactory redisConnectionFactory() throws URISyntaxException {
+        URI redisUri = new URI(System.getenv("REDISCLOUD_URL"));
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisUri.getHost());
+        config.setPort(redisUri.getPort());
+        if (redisUri.getUserInfo() != null) {
+            String password = redisUri.getUserInfo().split(":", 2)[1];
+            config.setPassword(password);
+        }
+
+        return new LettuceConnectionFactory(config);
     }
+
+
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(LettuceConnectionFactory connectionFactory) {

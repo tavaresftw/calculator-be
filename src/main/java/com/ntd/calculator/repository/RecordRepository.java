0f8b67f9676c27aad2584e8b3149485218514a9a
Jair Alves Tavares Junior <junior.age@gmail.com>
2024-09-03 01:07:31 -0300

    adjust on query

--- src/main/java/com/ntd/calculator/repository/RecordRepository.java
+++ src/main/java/com/ntd/calculator/repository/RecordRepository.java
@@ -9,10 +9,10 @@ import org.springframework.data.jpa.repository.Query;
 public interface RecordRepository extends JpaRepository<Record, Long> {
     @Query("SELECT r FROM Record r WHERE r.user.id = :userId AND r.deleted = false " +
             "AND (:search IS NULL OR :search = '' OR (" +
-            "LOWER(r.operationResponse) LIKE LOWER(:search) OR " +
-            "LOWER(r.operation.type) LIKE LOWER(:search) OR " +
-            "CAST(r.amount AS string) LIKE LOWER(:search) OR " +
-            "CAST(r.userBalance AS string) LIKE LOWER(:search) OR " +
-            "CAST(r.date AS string) LIKE LOWER(:search)))")
+            "LOWER(r.operationResponse) LIKE LOWER(CONCAT(:search, '%')) OR " +
+            "LOWER(r.operation.type) LIKE LOWER(CONCAT(:search, '%')) OR " +
+            "CAST(r.amount AS string) LIKE LOWER(CONCAT(:search, '%')) OR " +
+            "CAST(r.userBalance AS string) LIKE LOWER(CONCAT(:search, '%')) OR " +
+            "CAST(r.date AS string) LIKE LOWER(CONCAT(:search, '%'))))")
     Page<Record> findRecordByUserIdAndFilter(Long userId, String search, Pageable pageable);
 }
